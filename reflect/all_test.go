3ea0d299a5b240bd855d6e0c06f4c0ca27491e99
anlhord <anlhord@gmail.com>
2014-12-09 03:02:09 +0100

    The reflect package wraps and unwraps arbitrary slice type.
    To and from generic.Value

---
+++ reflect/all_test.go
@@ -0,0 +1,44 @@
+package unsafe
+
+import (
+	"example.com/repo.git/generic"
+	"testing"
+)
+
+type Wrapper interface {
+	Wrap() generic.Value
+	Unwrap(generic.Value)
+}
+
+type hipster struct {
+	age       int
+	moustache bool
+	fixiebike   bool
+	glasses   bool
+	gender    byte
+}
+
+func TestWrapCustomSlice(t *testing.T) {
+	var comrades []hipster
+	friends := []hipster{{19, false, true, true, 'M'},
+		{21, true, false, true, 'M'},
+		{20, false, true, false, 'F'}}
+
+
+	e := Stuff(&friends)
+	Unstuff(&comrades, e)
+
+	if len(friends) != len(comrades) {
+		t.Fatal("Nope, len.")
+	}
+	if cap(friends) != cap(comrades) {
+		t.Fatal("Nope, cap.")
+	}
+
+	for i := range friends {
+		if friends[i] != comrades[i] {
+			t.Fatal("Nope.")
+		}
+	}
+
+}
---
+++ reflect/hack.go
@@ -0,0 +1,47 @@
+package unsafe
+
+import (
+	"example.com/repo.git/generic"
+	"unsafe"
+	"reflect"
+)
+
+type ptr uintptr
+
+type Stuffer interface {
+	Stuff(interface{}) generic.Value
+	Unstuff(interface{}, generic.Value)
+}
+
+// FIXME if gc eats our data, we may want to stretch the
+// byte slice size a bit :) to accomodate the larger size of Ints or other
+// types :)
+
+func (i ptr) wrap() (o generic.Value) {
+	p := (*reflect.SliceHeader)(unsafe.Pointer(i))
+	q := (*reflect.SliceHeader)(unsafe.Pointer(&o))
+
+	*q = *p
+
+	return
+}
+
+func (i ptr) unwrap(o generic.Value) {
+	p := (*reflect.SliceHeader)(unsafe.Pointer(i))
+	q := (*reflect.SliceHeader)(unsafe.Pointer(&o))
+
+	*p = *q
+}
+
+
+func Stuff(src interface{}) generic.Value {
+	return Get(src).wrap()
+}
+
+func Unstuff(dst interface{}, val generic.Value) {
+	Get(dst).unwrap(val)
+}
+
+func Get(i interface{}) ptr {
+	return ptr(reflect.ValueOf(i).Pointer())
+}
